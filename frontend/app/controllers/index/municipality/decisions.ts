874832ba4520891dbd562432159a240c5ffb370f
senne <sennebels@gmail.com>
2022-10-24 10:44:05 +0200

    feat: :rocket: deploying to server to get ready for new tika featurues

--- frontend/app/controllers/index/municipality/decisions.ts
+++ frontend/app/controllers/index/municipality/decisions.ts
@@ -1,4 +1,5 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import axios from 'axios';
@@ -8,6 +9,37 @@ import { Decision } from 'index';
 export default class MunicipalityDecisions extends Controller {
   @service declare municipalities: MunicipalitiesService;
   @tracked decisionData: Array<Decision> = [];
+  @tracked searchQuery: string = '';
+  @tracked termsArray: any = {
+    texts: [
+      {
+        text_id: 1,
+        text: '',
+      },
+    ],
+  };
+
+  @action getSearchPredictionVectors() {
+    axios
+      .post(
+        'http://localhost:3000/embed-search-term/predictions/EmbedBert/1.0',
+        {
+          texts: [
+            {
+              text_id: 1,
+              text: this.searchQuery,
+            },
+          ],
+        }
+      )
+      .then((response) => {
+        this.getSearchQuerriesFromVectors(response.data);
+      });
+  }
+
+  @action getSearchQuerriesFromVectors(searchPredictionVectors: any) {
+    console.log(searchPredictionVectors);
+  }
 
   @tracked findMunicipalities = () => {
     axios
